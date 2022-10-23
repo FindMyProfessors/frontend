c6271d822d205d87578661f950bcad8ba96d36b1
Caleb <calebrossr@gmail.com>
2022-10-23 14:58:53 -0400

    Polish home page

--- src/modules/layout/Layout.tsx
+++ src/modules/layout/Layout.tsx
@@ -7,9 +7,9 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Navbar />
-      <div className="px-4">{children}</div>
+      {children}
       <Footer />
     </div>
   );
