63168ab7eb62aa82f1f1edcc19d0f20699ca9a5b
Warren Snipes <contact@warrensnipes.dev>
2022-11-15 00:37:39 -0500

    added generateBuildId for unique id based on git commit hash

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
+const execSync = require("child_process").execSync;
+
+const lastCommitCommand = "git rev-parse HEAD";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true },
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return execSync(lastCommitCommand).toString().trim();
+  },
 };
 
 module.exports = nextConfig;
